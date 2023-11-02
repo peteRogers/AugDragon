c6bc036bcb3561b93cec5895080b6f62d2681e6b
Peter Rogers <p.rogers@gold.ac.uk>
2023-11-02 15:53:08 +0000

    Initial Commit

---
+++ AugDragon/AugDragonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AugDragonApp.swift
+//  AugDragon
+//
+//  Created by Peter Rogers on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AugDragonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
